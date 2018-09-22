3f90353f306711eae03f0fcf3c12efab272536b4
C.y <yangruihan@vip.qq.com>
2018-09-22 08:32:40 +0800

    1. 新增 Vector4

--- Common/math.py
+++ Common/math.py
@@ -16,9 +16,9 @@ class Vector2:
         if args_len == 0:
             (self.x, self.y) = Vector2.zero()
         elif args_len == 1:
-            (self.x, self.y) = (args[0], args[0])
+            (self.x, self.y) = [args[0]] * 2
         elif args_len == 2:
-            (self.x, self.y) = (args[0], args[1])
+            (self.x, self.y) = args
         else:
             raise AttributeError
 
@@ -129,11 +129,14 @@ class Vector3:
         if args_len == 0:
             (self.x, self.y, self.z) = Vector3.zero()
         elif args_len == 1:
-            (self.x, self.y, self.z) = (args[0], args[0], args[0])
+            (self.x, self.y, self.z) = [args[0]] * 3
         elif args_len == 2:
-            (self.x, self.y, self.z) = (args[0], args[1], 0)
+            if isinstance(args[0], Vector2):
+                (self.x, self.y, self.z) = (args[0].x, args[0].y, args[1])
+            else:
+                (self.x, self.y, self.z) = (args[0], args[1], 0)
         elif args_len == 3:
-            (self.x, self.y, self.z) = (args[0], args[1], args[2])
+            (self.x, self.y, self.z) = args
         else:
             raise AttributeError
 
@@ -254,6 +257,121 @@ class Vector3:
         if cls._Zero is None:
             cls._Zero = Vector3(0)
         return cls._Zero
+
+
+class Vector4:
+    _Zero = None
+
+    def __init__(self, *args):
+        args_len = len(args)
+        if args_len == 0:
+            (self.x, self.y, self.z, self.w) = Vector4.zero()
+        elif args_len == 1:
+            (self.x, self.y, self.z, self.w) = [args[0]] * 4
+        elif args_len == 2:
+            if isinstance(args[0], Vector3):
+                (self.x, self.y, self.z, self.w) = (args[0].x, args[0].y, args[0].z, args[1])
+            else:
+                (self.x, self.y, self.z, self.w) = (args[0], args[1], 0, 0)
+        elif args_len == 3:
+            (self.x, self.y, self.z, self.w) = (args[0], args[1], args[2], 0)
+        elif args_len == 4:
+            (self.x, self.y, self.z, self.w) = args
+        else:
+            raise AttributeError
+
+    def __str__(self):
+        return 'Vector4(%s, %s, %s, %s)' % (self.x, self.y, self.z, self.w)
+
+    def __getitem__(self, item):
+        if isinstance(item, int):
+            if item == 0:
+                return self.x
+            elif item == 1:
+                return self.y
+            elif item == 2:
+                return self.z
+            elif item == 3:
+                return self.w
+            else:
+                raise IndexError
+        elif isinstance(item, str):
+            if item.lower() == 'x':
+                return self.x
+            elif item.lower() == 'y':
+                return self.y
+            elif item.lower() == 'z':
+                return self.z
+            elif item.lower() == 'w':
+                return self.w
+            else:
+                raise AttributeError
+        else:
+            raise AttributeError
+
+    def __add__(self, other):
+        assert isinstance(other, Vector4)
+
+        return Vector4(self.x + other.x,
+                       self.y + other.y,
+                       self.z + other.z,
+                       self.w + other.w)
+
+    def __sub__(self, other):
+        assert isinstance(other, Vector4)
+
+        return Vector3(self.x - other.x,
+                       self.y - other.y,
+                       self.z - other.z,
+                       self.w - other.w)
+
+    def __mul__(self, other):
+        try:
+            s = float(other)
+            return Vector4(self.x * s,
+                           self.y * s,
+                           self.z * s,
+                           self.w * s)
+        except Exception:
+            raise TypeError
+
+    def __truediv__(self, other):
+        try:
+            s = float(other)
+            return Vector4(self.x / s,
+                           self.y / s,
+                           self.z / s,
+                           self.w / s)
+        except Exception:
+            raise TypeError
+
+    @staticmethod
+    def dot(v1, v2):
+        if not isinstance(v1, Vector4) \
+                or not isinstance(v2, Vector4):
+            raise TypeError
+
+        return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z + v1.w * v2.w
+
+    def magnitude(self):
+        return sqrt(self.x * self.x + self.y * self.y + self.z * self.z + self.w * self.w)
+
+    def sqr_magnitude(self):
+        return self.x * self.x + self.y * self.y + self.z * self.z + self.w * self.w
+
+    def normalized(self):
+        tmp = 1 / self.magnitude()
+        return Vector3(self.x * tmp,
+                       self.y * tmp,
+                       self.z * tmp,
+                       self.w * tmp)
+
+    def normalize(self):
+        tmp = 1 / self.magnitude()
+        self.x *= tmp
+        self.y *= tmp
+        self.z *= tmp
+        self.w *= tmp
 
 
 class Quaternion:
@@ -444,7 +562,9 @@ class Quaternion:
 
 
 class Matrix4x4:
-    pass
+
+    def __init__(self, *args):
+        pass
 
 
 if __name__ == '__main__':
