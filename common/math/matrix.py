95790abb2c408035be8a2e32644c18044711e96a
C.y <yangruihan@vip.qq.com>
2018-09-22 20:55:57 +0800

    1. 完善Matrix3x3 2. 增加测试样例

--- common/math/matrix.py
+++ common/math/matrix.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 
 
-from common.math.vector import Vector2
+from common.math.vector import Vector2, Vector3
 
 EPSILON = 0.00001
 
@@ -16,10 +16,10 @@ class Matrix2x2:
         self.set(*args)
 
     def __str__(self):
-        return "Matrix2x2[[%s, %s], [%s, %s]]" % (self._element[0],
-                                                  self._element[1],
-                                                  self._element[2],
-                                                  self._element[3])
+        return "Matrix2x2[[%s, %s], [%s, %s]]" % (self[0],
+                                                  self[1],
+                                                  self[2],
+                                                  self[3])
 
     def __getitem__(self, item):
         if isinstance(item, int):
@@ -57,12 +57,12 @@ class Matrix2x2:
                 s = float(other)
                 self._element = [x * s for x in self._element]
             except ValueError:
-                pass
+                raise AttributeError
 
     def __eq__(self, other):
         if isinstance(other, Matrix2x2):
             for i in range(self.ELEMENT_COUNT):
-                if self._element[i] != other._element[i]:
+                if self[i] != other[i]:
                     return False
             return True
         else:
@@ -88,8 +88,8 @@ class Matrix2x2:
             raise AttributeError
 
     def get(self, x, y):
-        if x + y * 2 < self.ELEMENT_COUNT:
-            return self._element[x + y * 2]
+        if x + y * self.COL_COUNT < self.ELEMENT_COUNT:
+            return self[x + y * self.COL_COUNT]
 
     def set_row(self, index, values):
         for i in range(self.COL_COUNT):
@@ -102,13 +102,13 @@ class Matrix2x2:
     def get_row(self, index):
         ret = []
         for i in range(self.COL_COUNT):
-            ret.append(self._element[i + index * self.COL_COUNT])
+            ret.append(self[i + index * self.COL_COUNT])
         return ret
 
     def get_col(self, index):
         ret = []
         for i in range(self.ROW_COUNT):
-            ret.append(self._element[index + i * self.COL_COUNT])
+            ret.append(self[index + i * self.COL_COUNT])
         return ret
 
     def transpose(self):
@@ -122,7 +122,7 @@ class Matrix2x2:
         """
         行列式
         """
-        return self._element[0] * self._element[3] - self._element[1] * self._element[2]
+        return self[0] * self[3] - self[1] * self[2]
 
     def invert(self):
         """
@@ -135,10 +135,10 @@ class Matrix2x2:
         (self._element[0],
          self._element[1],
          self._element[2],
-         self._element[3]) = (inv_determinant * self._element[3],
-                              -inv_determinant * self._element[1],
-                              -inv_determinant * self._element[2],
-                              inv_determinant * self._element[0])
+         self._element[3]) = (inv_determinant * self[3],
+                              -inv_determinant * self[1],
+                              -inv_determinant * self[2],
+                              inv_determinant * self[0])
         return self
 
     @classmethod
@@ -147,9 +147,158 @@ class Matrix2x2:
 
 
 class Matrix3x3:
+    ELEMENT_COUNT = 9
+    ROW_COUNT = 3
+    COL_COUNT = 3
 
     def __init__(self, *args):
-        pass
+        self._element = [0] * self.ELEMENT_COUNT
+        self.set(*args)
+
+    def __str__(self):
+        return "Matrix3x3[[%s, %s, %s], [%s, %s, %s], [%s, %s, %s]]" % \
+               (self[0], self[1], self[2],
+                self[3], self[4], self[5],
+                self[6], self[7], self[8])
+
+    def __getitem__(self, item):
+        if isinstance(item, int):
+            return self._element[item]
+
+    def __add__(self, other):
+        if isinstance(other, Matrix3x3):
+            return Matrix3x3(self[0] + other[0], self[1] + other[1], self[2] + other[2],
+                             self[3] + other[3], self[4] + other[4], self[5] + other[5],
+                             self[6] + other[6], self[7] + other[7], self[8] + other[8])
+
+    def __sub__(self, other):
+        if isinstance(other, Matrix3x3):
+            return Matrix3x3(self[0] - other[0], self[1] - other[1], self[2] - other[2],
+                             self[3] - other[3], self[4] - other[4], self[5] - other[5],
+                             self[6] - other[6], self[7] - other[7], self[8] - other[8])
+
+    def __mul__(self, other):
+        if isinstance(other, Matrix3x3):
+            ret = [0] * self.ELEMENT_COUNT
+            for row in range(self.ROW_COUNT):
+                for col in range(self.COL_COUNT):
+                    sum_value = 0
+                    for i in range(self.ROW_COUNT):
+                        sum_value += self.get(i, row) * other.get(col, i)
+                    ret[col + row * self.COL_COUNT] = sum_value
+            return Matrix3x3(ret)
+        elif isinstance(other, Vector3):
+            return Vector3(self[0] * other.x + self[1] * other.y + self[2] * other.z,
+                           self[3] * other.x + self[4] * other.y + self[5] * other.z,
+                           self[6] * other.x + self[7] * other.y + self[8] * other.z)
+        else:
+            try:
+                s = float(other)
+                self._element = [x * s for x in self._element]
+            except ValueError:
+                raise AttributeError
+
+    def __eq__(self, other):
+        if isinstance(other, Matrix3x3):
+            for i in range(self.ELEMENT_COUNT):
+                if self[i] != other._element[i]:
+                    return False
+            return True
+        else:
+            return False
+
+    def set(self, *args):
+        args_len = len(args)
+        if args_len == 0:
+            pass
+        elif args_len == 1:
+            if isinstance(args[0], list) or isinstance(args[0], tuple):
+                self._element = [_ for _ in args[0]]
+            else:
+                self._element = [0] * self.ELEMENT_COUNT
+                for i in range(self.ROW_COUNT):
+                    self._element[i + i * self.COL_COUNT] = args[0]
+        elif args_len == 3 \
+                and isinstance(args[0], Vector3) \
+                and isinstance(args[1], Vector3) \
+                and isinstance(args[2], Vector3):
+            self._element = [args[0].x, args[0].y, args[0].z,
+                             args[1].x, args[1].y, args[1].z,
+                             args[2].x, args[2].y, args[2].z]
+        elif args_len == 9:
+            self._element = list(args)
+        else:
+            raise AttributeError
+
+    def get(self, x, y):
+        if x + y * self.COL_COUNT < self.ELEMENT_COUNT:
+            return self[x + y * self.COL_COUNT]
+
+    def set_row(self, index, values):
+        for i in range(self.COL_COUNT):
+            self._element[i + index * self.COL_COUNT] = values[i]
+
+    def set_col(self, index, values):
+        for i in range(self.ROW_COUNT):
+            self._element[index + i * self.COL_COUNT] = values[i]
+
+    def get_row(self, index):
+        ret = []
+        for i in range(self.COL_COUNT):
+            ret.append(self[i + index * self.COL_COUNT])
+        return ret
+
+    def get_col(self, index):
+        ret = []
+        for i in range(self.ROW_COUNT):
+            ret.append(self[index + i * self.COL_COUNT])
+        return ret
+
+    def transpose(self):
+        """
+        转置矩阵
+        """
+        (self._element[1], self._element[3]) = (self._element[3], self._element[1])
+        (self._element[2], self._element[6]) = (self._element[6], self._element[2])
+        (self._element[5], self._element[7]) = (self._element[7], self._element[5])
+        return self
+
+    def determinant(self):
+        """
+        行列式
+        """
+        return self[0] * (self[4] * self[8] - self[5] * self[7]) - \
+               self[1] * (self[3] * self[8] - self[5] * self[6]) + \
+               self[2] * (self[3] * self[7] - self[4] * self[6])
+
+    def invert(self):
+        """
+        逆矩阵
+        """
+        determinant = self.determinant()
+        if determinant <= EPSILON:
+            return Matrix3x3.identity()
+
+        tmp = [0] * self.ELEMENT_COUNT
+        tmp[0] = self[4] * self[8] - self[5] * self[7]
+        tmp[1] = self[2] * self[7] - self[1] * self[8]
+        tmp[2] = self[1] * self[5] - self[2] * self[4]
+        tmp[3] = self[5] * self[6] - self[3] * self[8]
+        tmp[4] = self[0] * self[8] - self[2] * self[6]
+        tmp[5] = self[2] * self[3] - self[0] * self[5]
+        tmp[6] = self[3] * self[7] - self[4] * self[6]
+        tmp[7] = self[1] * self[6] - self[0] * self[7]
+        tmp[8] = self[0] * self[4] - self[1] * self[3]
+
+        inv_determinant = 1 / determinant
+        for i in range(self.ELEMENT_COUNT):
+            self._element[i] = inv_determinant * tmp[i]
+
+        return self
+
+    @classmethod
+    def identity(cls):
+        return Matrix3x3(1)
 
 
 class Matrix4x4:
--- test.py
+++ test.py
@@ -2,8 +2,8 @@
 
 
 import unittest
-from common.math.matrix import Matrix2x2
-from common.math.vector import Vector2
+from common.math.matrix import Matrix2x2, Matrix3x3
+from common.math.vector import Vector2, Vector3
 
 
 class TestMatrix2x2Methods(unittest.TestCase):
@@ -58,6 +58,60 @@ class TestMatrix2x2Methods(unittest.TestCase):
         tmp = Matrix2x2([1, 2, 3, 4])
         tmp.transpose()
         self.assertEqual(str(tmp), 'Matrix2x2[[1, 3], [2, 4]]')
+
+
+class TestMatrix3x3Methods(unittest.TestCase):
+
+    def setUp(self):
+        self.m = Matrix3x3()
+        self.m1 = Matrix3x3([1, 2, 3, 4, 5, 6, 7, 8, 9])
+        self.m2 = Matrix3x3((5, 6, 7, 8, 9, 10, 11, 12, 13))
+        self.m3 = Matrix3x3(5)
+        self.m4 = Matrix3x3(Vector3(1, 2, 3), Vector3(4, 5, 6), Vector3(7, 8, 9))
+        self.m5 = Matrix3x3(1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+    def test_init(self):
+        self.assertEqual(self.m[0], 0)
+        self.assertEqual(self.m[1], 0)
+        self.assertEqual(self.m[2], 0)
+        self.assertEqual(self.m[3], 0)
+        self.assertEqual(str(self.m), 'Matrix3x3[[0, 0, 0], [0, 0, 0], [0, 0, 0]]')
+        self.assertEqual(str(self.m1), 'Matrix3x3[[1, 2, 3], [4, 5, 6], [7, 8, 9]]')
+        self.assertEqual(str(self.m2), 'Matrix3x3[[5, 6, 7], [8, 9, 10], [11, 12, 13]]')
+        self.assertEqual(str(self.m3), 'Matrix3x3[[5, 0, 0], [0, 5, 0], [0, 0, 5]]')
+        self.assertEqual(str(self.m4), 'Matrix3x3[[1, 2, 3], [4, 5, 6], [7, 8, 9]]')
+        self.assertEqual(str(self.m5), 'Matrix3x3[[1, 2, 3], [4, 5, 6], [7, 8, 9]]')
+
+    def test_get(self):
+        self.assertEqual(self.m1.get(0, 1), 4)
+        self.assertEqual(self.m3.get(1, 1), 5)
+
+    def test_identity(self):
+        self.assertEqual(str(Matrix3x3.identity()), 'Matrix3x3[[1, 0, 0], [0, 1, 0], [0, 0, 1]]')
+
+    def test_get_item(self):
+        self.assertEqual(self.m1[0], 1)
+        self.assertEqual(self.m5[3], 4)
+
+    def test_add(self):
+        tmp = self.m1 + self.m2
+        self.assertEqual(str(tmp), 'Matrix3x3[[6, 8, 10], [12, 14, 16], [18, 20, 22]]')
+
+    def test_sub(self):
+        tmp = self.m2 - self.m1
+        self.assertEqual(str(tmp), 'Matrix3x3[[4, 4, 4], [4, 4, 4], [4, 4, 4]]')
+
+    def test_mul(self):
+        tmp = self.m1 * self.m2
+        self.assertEqual(str(tmp), 'Matrix3x3[[54, 60, 66], [126, 141, 156], [198, 222, 246]]')
+
+    def test_eq(self):
+        self.assertTrue(self.m1 == self.m5)
+
+    def test_transpose(self):
+        tmp = Matrix3x3([1, 2, 3, 4, 5, 6, 7, 8, 9])
+        tmp.transpose()
+        self.assertEqual(str(tmp), 'Matrix3x3[[1, 4, 7], [2, 5, 8], [3, 6, 9]]')
 
 
 if __name__ == '__main__':
